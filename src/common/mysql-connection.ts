8bd2715d5150b635b4c24c9b74d7b7b4d2c60097
Наталія Тюх <nataliatuh@Natalias-MacBook-Air.local>
2022-11-17 15:35:18 +0100

    mysql connection changed

--- src/common/mysql-connection.ts
+++ src/common/mysql-connection.ts
@@ -1,8 +1,9 @@
 import * as mysql from 'mysql2/promise'
 
 export const connect = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    database: "users_list",
-    password: 'root'
+    host: "db4free.net",
+    user: "natasha",
+    database: "application",
+    password: 'vjiQm24RqoTgvi7ZoYuq',
+    connectTimeout: 1000 * 60 * 5
 })
